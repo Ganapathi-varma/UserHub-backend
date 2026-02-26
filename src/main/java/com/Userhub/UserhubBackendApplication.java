e51b750430ce15571f00ed32685088f60e993857
Ganapathi-varma <vinayvarma508@gmail.com>
2026-02-26 09:13:34 +0530

    Create UserhubBackendApplication.java

---
+++ src/main/java/com/Userhub/UserhubBackendApplication.java
@@ -0,0 +1,12 @@
+package com.Userhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserhubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserhubBackendApplication.class, args);
+    }
+}
